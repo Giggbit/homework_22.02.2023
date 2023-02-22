b2cb3cfefc5a9bd5e88436240a943aead29a2418
Giggbit <danya19981017@gmail.com>
2023-02-22 18:20:50 +0100

    Добавьте файлы проекта.

---
+++ Bankomat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_homework_22._02._2023
+{
+    internal class Bankomat: Bill
+    {
+        private int sum;
+
+        public Bankomat() { }
+        public Bankomat(int pin, int sum) { 
+            this.sum = sum;
+        }
+
+        public void EnterSum(int sum) { 
+            Console.WriteLine($"Amount processed...\nIssuance amount: {sum} euro");
+        }
+    }
+}
---
+++ Bill.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_homework_22._02._2023
+{
+    internal class Bill
+    {
+        public Bill() { }
+        public void Print(int sum) { 
+            Console.WriteLine($"Issuance amount: {sum} euro");
+        }
+        public void NoPrint() {
+            Console.WriteLine("Bill will not printe");
+        }
+    }
+}
